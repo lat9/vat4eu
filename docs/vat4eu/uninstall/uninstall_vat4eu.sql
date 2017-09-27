bb7292561998d54bd127d1558596974538ab0b96
lat9 <lat9@vinosdefrutastropicales.com>
2017-09-27 14:55:00 -0400

    Add documentation and uninstall SQL
    ... to the readme.

---
+++ docs/vat4eu/uninstall/uninstall_vat4eu.sql
@@ -0,0 +1,5 @@
+DELETE FROM configuration WHERE configuration_key LIKE 'VAT4EU_%';
+DELETE FROM configuration_group WHERE configuration_group_title = 'VAT4EU Plugin' LIMIT 1;
+DELETE FROM admin_pages WHERE page_key IN ('configVat4Eu');
+ALTER TABLE address_book DROP COLUMN entry_vat_number, DROP COLUMN entry_vat_validated;
+ALTER TABLE orders DROP COLUMN billing_vat_number, DROP COLUMN billing_vat_validated;
