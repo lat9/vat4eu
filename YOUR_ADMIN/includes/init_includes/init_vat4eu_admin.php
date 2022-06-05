745dda7137386c4cb5f5e893b45d79e51a467e8c
lat9 <cindy@vinosdefrutastropicales.com>
2022-06-04 21:06:44 -0400

    #20: Correct 'hidden' text on vat-formatting help-text pop-up

--- YOUR_ADMIN/includes/init_includes/init_vat4eu_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_vat4eu_admin.php
@@ -7,8 +7,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
 
-define('VAT4EU_CURRENT_RELEASE', '3.1.1');
-define('VAT4EU_CURRENT_UPDATE_DATE', '2022-04-28');
+define('VAT4EU_CURRENT_RELEASE', '3.1.2-beta1');
+define('VAT4EU_CURRENT_UPDATE_DATE', '2022-06-04');
 
 define('VAT4EU_CURRENT_VERSION', VAT4EU_CURRENT_RELEASE . ': ' . VAT4EU_CURRENT_UPDATE_DATE);
 
