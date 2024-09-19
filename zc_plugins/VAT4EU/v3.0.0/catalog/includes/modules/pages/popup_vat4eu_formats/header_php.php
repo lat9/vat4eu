69e355c3653367b1e0795ffbb2720f250c1e2799
lat9 <lat9@vinosdefrutastropicales.com>
2024-09-19 15:23:38 -0400

    Remove to-be-determined storefront files

--- zc_plugins/VAT4EU/v3.0.0/catalog/includes/modules/pages/popup_vat4eu_formats/header_php.php
+++
@@ -1,8 +0,0 @@
-<?php
-// -----
-// Part of the VAT4EU plugin by Cindy Merkin a.k.a. lat9 (cindy@vinosdefrutastropicales.com)
-// Copyright (c) 2017 Vinos de Frutas Tropicales
-//
-$_SESSION['navigation']->remove_current_page();
-require DIR_WS_MODULES . zen_get_module_directory('require_languages.php');
-$define_page = zen_get_file_directory(DIR_WS_LANGUAGES . $_SESSION['language'] . '/html_includes/', FILENAME_DEFINE_POPUP_VAT4EU_FORMATS, 'false');
